b1fad1898092bbaf259cd5c77521bcd4e7e267d4
Camila Fimbres <camifimbres@gmail.com>
2024-10-11 17:21:07 -0700

    Agregar archivos de proyecto.

---
+++ CalificacionesEv/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+namespace CalificacionesEv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Bienvenido querido estudiante, introduzca su calificación :3");
+            int calificacion = int.Parse(Console.ReadLine());
+            Console.WriteLine(calificacion);
+
+            if (calificacion >=70)
+            {
+                Console.WriteLine($"Mayores que 60");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Aprobado");
+            }
+            else if (calificacion <=50)
+            {
+                Console.WriteLine($"Menores que 60");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Reprobado");
+            }
+            else 
+            {
+                Console.WriteLine("60");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Regularización");
+            }
+        }
+    }
+}
